f6ca95d1ac12d96ed5d8816672eebd4b8dd2a5f4
Yeremiayoga <yogayeremia@gmail.com>
2023-03-04 20:43:54 +0700

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,133 +3,133 @@
     <div class="">
       <div class="flex ">
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>23</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>22</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 1" src="../assets/circ-full-putih-border.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 1" src="../assets/circ-full-putih-border.png"
             @click="circle1met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 2" src="../assets/circ-14-kuning.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 2" src="../assets/circ-14-kuning.png"
             @click="circle1met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 3" src="../assets/circ-half-hijau.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 3" src="../assets/circ-half-hijau.png"
             @click="circle1met" />
-          <img style="width: 100px; padding: 10px; transform: rotate(-90deg)" v-if="circle1 == 4" src="../assets/circ-34-merah.png"
+          <img style="width: 85px; padding: 10px; transform: rotate(-90deg)" v-if="circle1 == 4"
+            src="../assets/circ-34-merah.png" @click="circle1met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 5" src="../assets/circ-full-biru.png"
             @click="circle1met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 5" src="../assets/circ-full-biru.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 6" src="../assets/circ-full-biru-bintang.png"
             @click="circle1met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 6" src="../assets/circ-full-biru-bintang.png"
-            @click="circle1met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle1 == 7" src="../assets/circ-full-hitam.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle1 == 7" src="../assets/circ-full-hitam.png"
             @click="circle1met" />
           <p>21</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 1" src="../assets/circ-full-putih-border.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 2" src="../assets/circ-14-kuning.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 3" src="../assets/circ-half-hijau.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px; transform: rotate(-90deg)" v-if="circle2 == 4" src="../assets/circ-34-merah.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 5" src="../assets/circ-full-biru.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 6" src="../assets/circ-full-biru-bintang.png" @click="circle2met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle2 == 7" src="../assets/circ-full-hitam.png" @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 1" src="../assets/circ-full-putih-border.png"
+            @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 2" src="../assets/circ-14-kuning.png"
+            @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 3" src="../assets/circ-half-hijau.png"
+            @click="circle2met" />
+          <img style="width: 85px; padding: 10px; transform: rotate(-90deg)" v-if="circle2 == 4"
+            src="../assets/circ-34-merah.png" @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 5" src="../assets/circ-full-biru.png"
+            @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 6" src="../assets/circ-full-biru-bintang.png"
+            @click="circle2met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle2 == 7" src="../assets/circ-full-hitam.png"
+            @click="circle2met" />
           <p>20</p>
         </div>
       </div>
       <div class="flex">
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>26</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>27</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 1" src="../assets/circ-full-putih-border.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 1" src="../assets/circ-full-putih-border.png"
             @click="circle3met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 2" src="../assets/circ-14-kuning.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 2" src="../assets/circ-14-kuning.png"
             @click="circle3met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 3" src="../assets/circ-half-hijau.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 3" src="../assets/circ-half-hijau.png"
             @click="circle3met" />
-          <img style="width: 100px; padding: 10px; transform: rotate(-90deg)" v-if="circle3 == 4" src="../assets/circ-34-merah.png"
+          <img style="width: 85px; padding: 10px; transform: rotate(-90deg)" v-if="circle3 == 4"
+            src="../assets/circ-34-merah.png" @click="circle3met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 5" src="../assets/circ-full-biru.png"
             @click="circle3met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 5" src="../assets/circ-full-biru.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 6" src="../assets/circ-full-biru-bintang.png"
             @click="circle3met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 6" src="../assets/circ-full-biru-bintang.png"
-            @click="circle3met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle3 == 7" src="../assets/circ-full-hitam.png"
+          <img style="width: 85px; padding: 10px;" v-if="circle3 == 7" src="../assets/circ-full-hitam.png"
             @click="circle3met" />
           <p>28</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 1" src="../assets/circ-full-putih-border.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 2" src="../assets/circ-14-kuning.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 3" src="../assets/circ-half-hijau.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px; transform: rotate(-90deg)" v-if="circle4 == 4" src="../assets/circ-34-merah.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 5" src="../assets/circ-full-biru.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 6" src="../assets/circ-full-biru-bintang.png" @click="circle4met" />
-          <img style="width: 100px; padding: 10px;" v-if="circle4 == 7" src="../assets/circ-full-hitam.png" @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 1" src="../assets/circ-full-putih-border.png"
+            @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 2" src="../assets/circ-14-kuning.png"
+            @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 3" src="../assets/circ-half-hijau.png"
+            @click="circle4met" />
+          <img style="width: 85px; padding: 10px; transform: rotate(-90deg)" v-if="circle4 == 4"
+            src="../assets/circ-34-merah.png" @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 5" src="../assets/circ-full-biru.png"
+            @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 6" src="../assets/circ-full-biru-bintang.png"
+            @click="circle4met" />
+          <img style="width: 85px; padding: 10px;" v-if="circle4 == 7" src="../assets/circ-full-hitam.png"
+            @click="circle4met" />
           <p>29</p>
         </div>
 
       </div>
       <div class="flex">
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>35</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>34</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>33</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>32</p>
         </div>
 
       </div>
       <div class="flex">
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>38</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>39</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>40</p>
         </div>
         <div>
-          <img style="width: 100px; padding: 10px;" src="../assets/circ-full-abu.png" />
+          <img style="width: 85px; padding: 10px;" src="../assets/circ-full-abu.png" />
           <p>41</p>
         </div>
-        
+
       </div>
-      
+
     </div>
-    <div class="flex" style="margin-top: 30px;">
-        <div>
-          <img style="width: 100px; padding: 30px; " src="../assets/circ-full-biru.png" />
-        </div>
-        <div>
-          <img style="width: 100px; padding: 30px; " src="../assets/circ-full-biru-bintang.png" />
-        </div>
-        <div>
-          <img style="width: 100px; padding: 30px; " src="../assets/circ-full-hitam.png" />
-        </div>
-      </div>
-
-
-
   </div>
 </template>
 
@@ -182,8 +182,9 @@ export default {
 .flex {
   display: flex;
   justify-content: center;
-  
+
 }
+
 .cicle {
   width: 100px;
   height: 100px;
